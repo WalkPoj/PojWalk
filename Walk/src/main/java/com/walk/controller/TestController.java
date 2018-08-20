33652827e55b7230c01f2c462c2f4873d17bf182
sjn <1527561632@qq.com>
2018-08-20 21:40:30 +0800

    sjn

--- Walk/src/main/java/com/walk/controller/TestController.java
+++ Walk/src/main/java/com/walk/controller/TestController.java
@@ -11,7 +11,7 @@ public class TestController {
     @RequestMapping("/test")
     public String test(Model mod){
         mod.addAttribute("index","a");
-        System.out.println("aaa");
+        System.out.println("aaaa");
         return "model";
     }
 }
