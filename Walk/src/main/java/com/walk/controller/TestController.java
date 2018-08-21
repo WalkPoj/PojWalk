34ca845f87c78865c82031a00675070438de3d83
sjn <1527561632@qq.com>
2018-08-21 08:49:37 +0800

    TestController中的syso修改为aaawaa

--- Walk/src/main/java/com/walk/controller/TestController.java
+++ Walk/src/main/java/com/walk/controller/TestController.java
@@ -11,7 +11,7 @@ public class TestController {
     @RequestMapping("/test")
     public String test(Model mod){
         mod.addAttribute("index","a");
-        System.out.println("aaaa");
+        System.out.println("aaawaa");
         return "model";
     }
 }
