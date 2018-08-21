9fc93b4c3915fe1f409b6c9abc90caaf0380750f
zz <2256779924@qq.com>
2018-08-21 09:21:40 +0800

    加入 后台模块

---
+++ walkBAM/src/main/java/com/walkBAM/util/ErrorInterceptor.java
@@ -0,0 +1,19 @@
+package com.walkBAM.util;
+
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class ErrorInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        System.out.println("进入拦截器");
+        return false;
+    }
+
+}
