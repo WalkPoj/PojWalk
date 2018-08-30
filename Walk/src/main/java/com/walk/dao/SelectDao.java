d7bb5f5dfdebd5bfe1b84b41f62af5a7d7a5a77c
zz <zzlovey@foxmail.com>
2018-08-31 00:38:44 +0800

    修复分页查询

--- Walk/src/main/java/com/walk/dao/SelectDao.java
+++ Walk/src/main/java/com/walk/dao/SelectDao.java
@@ -9,6 +9,6 @@ public interface SelectDao {
     public List<Scenery> indexSelect();
 
     //分类查询
-    public List<Scenery> listView(int s_num,int price,String address);
+    public List<Scenery> listView();
 }
 
