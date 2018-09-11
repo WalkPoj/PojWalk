53b4acc403ee1a84aa3c9b98de3c7d541b8cf54d
AxiangDadi <421681437@qq.com>
2018-09-11 20:20:51 +0800

    完成订单，加入新的样式

--- Walk/src/main/java/com/walk/controller/SelectController.java
+++ Walk/src/main/java/com/walk/controller/SelectController.java
@@ -155,6 +155,7 @@ public class SelectController {
      */
     @PostMapping("SaveOrder")
     public String SaveOrder(HttpSession session,Order_info oi){
+        System.out.println(oi.getS_id());
         //生成订单号
         String Order_id = FileUtil.getOrderIdByUUId();
         //获取用户id
