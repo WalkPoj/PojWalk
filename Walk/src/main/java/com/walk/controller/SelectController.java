b3adae083d29c623071f2c2cefef7cd1cc6b4eaf
AxiangDadi <421681437@qq.com>
2018-09-13 19:05:24 +0800

    优化页面加载速度

--- Walk/src/main/java/com/walk/controller/SelectController.java
+++ Walk/src/main/java/com/walk/controller/SelectController.java
@@ -31,7 +31,6 @@ public class SelectController {
     @Autowired
     private RedisTemplate redisTemplate;
 
-
     /**
      * 查询首页推荐
      * @param mod
@@ -45,6 +44,10 @@ public class SelectController {
         return "cpts_398_pn/deals";
     }
 
+    @RequestMapping("listView.html")
+    public String lvhtml(){
+        return "listView";
+    }
     /**
      * 查看更多
      * @param mod
@@ -64,7 +67,73 @@ public class SelectController {
         System.out.println("第"+sPage+"页数据将在60后被销毁"+redisTemplate.expire("lvScenery::data_page_"+sPage,60,TimeUnit.SECONDS));
         return "cpts_398_pn/products";
     }
+    private Map<String,Integer> bing = new HashMap<>();
+    private int s_city = 0;
+    private int s_price = 0 ;
+    private int s_num = 0;
+    private List<Scenery> Shai = new ArrayList<>();
 
+
+    @PostMapping("Shaixuan")
+    public String Shaixuan(String Da, String Dc){
+        bing.put("评分",0);
+        bing.put("地区",0);
+        bing.put("价格区间",0);
+
+        String[] name = Da.split(":");
+        if (name[0].equals("评分")){
+            s_num = 1;
+            bing.put(name[0],s_num);
+        }else if (name[0].equals("地区")){
+            if (Dc.equals("张家界")){
+                s_city = 148;
+            }else if (Dc.equals("三亚")){
+                s_city = 101;
+            }else if (Dc.equals("厦门")){
+                s_city = 123;
+            }else if(Dc.equals("北京")){
+                s_city = 5;
+            }else if (Dc.equals("西安")){
+                s_city = 124;
+            }else if (Dc.equals("青岛")){
+                s_city = 93;
+            }else if (Dc.equals("贵州")){
+                s_city = 34;
+            }else if (Dc.equals("西藏")){
+                s_city = 125;
+            }
+            bing.put(name[0],s_city);
+        }else if (name[0].equals("价格区间")){
+            if (Dc.equals("0-1000")){
+                s_price = 999;
+            }else if (Dc.equals("1000-2500")){
+                s_price = 2499;
+            }else if (Dc.equals("2500-3500")){
+                s_price = 3499;
+            }else if (Dc.equals("5500-9500")){
+                s_price = 8499;
+            }else {
+                s_price = 9599;
+            }
+            bing.put(name[0],s_price);
+        }
+        Shai = sdaos.selectByMod(bing.get("地区"),bing.get("价格区间"),bing.get("评分"));
+        return "";
+    }
+
+    /**
+     * 取消条件
+     * @return
+     */
+    @PostMapping("Quxiao")
+    @ResponseBody
+    public List<Scenery> cacanleData(String Da) {
+        String[] name = Da.split(":");
+        bing.put(name[0],0);
+        System.out.println(bing.size());
+        Shai = sdaos.selectByMod(bing.get("地区"),bing.get("价格区间"),bing.get("评分"));
+        return Shai;
+    }
     /**
      * 加载详情页面
      * @return
@@ -142,8 +211,9 @@ public class SelectController {
      */
     @RequestMapping("listOrder")
     public String OrderHtml(HttpSession session,Model mod,Data_Order data_order){
-        System.out.println(data_order.getPerson_num());
         String u_id = (String)session.getAttribute("user_id");
+        System.out.println(data_order.getPerson_num());
+
         mod.addAttribute("Data_order",data_order);
         //添加班次id
         session.setAttribute("Data_INFO",(Data_Class_INFO)redisTemplate.opsForValue().get("Data_INFO_"+u_id));
@@ -187,72 +257,5 @@ public class SelectController {
         }
         return "index";
     }
-    private Map<String,Integer> bing = new HashMap<>();
-    private int s_city = 0;
-    private int s_price = 0 ;
-    private int s_num = 0;
-    private List<Scenery> Shai = new ArrayList<>();
 
-
-    @PostMapping("Shaixuan")
-    @ResponseBody
-    public List<Scenery> Shaixuan(String Da, String Dc){
-        bing.put("评分",0);
-        bing.put("地区",0);
-        bing.put("价格区间",0);
-
-        String[] name = Da.split(":");
-        if (name[0].equals("评分")){
-            s_num = 1;
-            bing.put(name[0],s_num);
-        }else if (name[0].equals("地区")){
-            if (Dc.equals("张家界")){
-                s_city = 148;
-            }else if (Dc.equals("三亚")){
-                s_city = 101;
-            }else if (Dc.equals("厦门")){
-                s_city = 123;
-            }else if(Dc.equals("北京")){
-                s_city = 5;
-            }else if (Dc.equals("西安")){
-                s_city = 124;
-            }else if (Dc.equals("青岛")){
-                s_city = 93;
-            }else if (Dc.equals("贵州")){
-                s_city = 34;
-            }else if (Dc.equals("西藏")){
-                s_city = 125;
-            }
-            bing.put(name[0],s_city);
-        }else if (name[0].equals("价格区间")){
-            if (Dc.equals("0-1000")){
-                s_price = 999;
-            }else if (Dc.equals("1000-2500")){
-                s_price = 2499;
-            }else if (Dc.equals("2500-3500")){
-                s_price = 3499;
-            }else if (Dc.equals("5500-9500")){
-                s_price = 8499;
-            }else {
-                s_price = 9599;
-            }
-            bing.put(name[0],s_price);
-        }
-        Shai = sdaos.selectByMod(bing.get("地区"),bing.get("价格区间"),bing.get("评分"));
-        return Shai;
-    }
-
-    /**
-     * 取消条件
-     * @return
-     */
-    @PostMapping("Quxiao")
-    @ResponseBody
-    public List<Scenery> cacanleData(String Da) {
-        String[] name = Da.split(":");
-        bing.put(name[0],0);
-        System.out.println(bing.size());
-        Shai = sdaos.selectByMod(bing.get("地区"),bing.get("价格区间"),bing.get("评分"));
-        return Shai;
-    }
 }
